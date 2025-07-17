567cf7c6e320ae2e82fa5afb1107d1a76d96d6df
fyllipraven <fyllipraven@gmail.com>
2025-07-16 20:09:01 -0400

    Reverted lora_setup settings

--- drone_code/lora_setup.py
+++ drone_code/lora_setup.py
@@ -13,9 +13,9 @@ def get_lora_radio():
 
     rfm9x = adafruit_rfm9x.RFM9x(spi, chip_select, reset, RADIO_FREQ_MHZ, baudrate=BAUD_RATE)
     rfm9x.tx_power = 23
-    rfm9x.signal_bandwidth = 500000
-    rfm9x.spreading_factor = 7
-    rfm9x.coding_rate = 5
-    rfm9x.enable_crc = True
+    #rfm9x.signal_bandwidth = 500000
+    #rfm9x.spreading_factor = 7
+    #rfm9x.coding_rate = 5
+    #rfm9x.enable_crc = True
     print("LoRa transceiver is initialized and tuned.")
     return rfm9x
